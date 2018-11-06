36db6f3c0f8ca3dc3a52dfd4eb5419a7b41afb70
Imani Christie <ichristie@spartaglobal.com>
2018-11-06 18:00:39 +0000

    v.1.0 Club MVC complete

---
+++ db/migrate/20181106151319_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :firstName
+      t.string :lastName
+      t.integer :club_id
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
