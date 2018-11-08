3a2637bcedd7556903345823e5713e5083f1a9ea
Imani Christie <ichristie@spartaglobal.com>
2018-11-08 16:17:59 +0000

    V.3.2 Deployment test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@ Club.create!([
   {name: "Watford FC", user_id: nil, location: "Watford", date_of_establishment: "1881-05-24"}
 ])
 Comment.create!([
-  {username: "top_tips", body: "We have done terribly this season", club_id: 6},
-  {username: "IballEveryday", body: "We need new signings honestly", club_id: 6},
-  {username: "HappyGooner", body: "I love the direction were going in", club_id: 6},
-  {username: "UnhappyGooner", body: "We need a new Keeper. All that money and we've spent and we are still conceeding goals", club_id: 1},
-  {username: "DatGuyWelbz", body: "Danny Laca and Auba need to play together", club_id: 1},
-  {username: "NoName", body: "We loook really strong going forward", club_id: 1},
-  {username: "JojoManson", body: "Huge plans for the club this season, hopefully all the fans get behind us. We will need the support", club_id: 1},
-  {username: "SunnyBoy79", body: "I have to beat the mid table teams this year. We have been dropping too many points on them!!", club_id: 8}
+  {username: "", body: "", club_id: 6},
+  {username: "", body: "", club_id: 6},
+  {username: "", body: "", club_id: 6},
+  {username: "", body: "", club_id: 1},
+  {username: "", body: "", club_id: 1},
+  {username: "", body: "", club_id: 1},
+  {username: "", body: "", club_id: 1},
+  {username: "", body: "", club_id: 1}
 ])
 Player.create!([
   {firstName: "Alex", lastName: "Iwobi", club_id: 1, age: 22, position: "Forward", nationality: "Nigeria"},
@@ -26,5 +26,6 @@ Player.create!([
   {firstName: "Eden", lastName: "Hazard", club_id: 6, age: 24, position: "Forward", nationality: "Belgium"},
   {firstName: "Mesut", lastName: "Ozil", club_id: 1, age: 30, position: "Forward", nationality: "Germany"},
   {firstName: "Charlie", lastName: "Adams", club_id: 8, age: 33, position: "Midfielder", nationality: "England"},
-  {firstName: "Wasteman", lastName: "Morata", club_id: 6, age: 23, position: "Forward", nationality: "Nigeria"}
+  {firstName: "Wasteman", lastName: "Morata", club_id: 6, age: 23, position: "Forward", nationality: "Nigeria"},
+  {firstName: "Morgan", lastName: "Kennedy", club_id: 12, age: 24, position: "Midfielder", nationality: "Spain"}
 ])
